1bb8c9b68942795c547f1086aa2cbfb2c1cf3106
Hanakus0 <everydoghas12day@outlook.jp>
2024-10-21 13:03:49 +0900

    UPDATE: Confirm header i18n

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,10 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # gem rails-i18n
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
+    # 複数の言語ファイルが読み込まれるようにする
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end

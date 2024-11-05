d7a109165b943df4d9a96265891772599ca0a8d1
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-05 23:00:47 +0900

    ADD: home page posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,30 @@
 class HomeController < ApplicationController
   def index
+    # ログイン後
     if user_signed_in?
-      @posts = Post.order("RANDOM()").limit(20)
+      # ランダム
+      @random_posts = Post.order("RANDOM()").limit(20)
+      add_blank_post(@random_posts)
+      # いいね数
+      @good_posts = Post.find(PostLike.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+      add_blank_post(@good_posts)
+      # コメント数
+      @comment_posts = Post.find(Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+      add_blank_post(@comment_posts)
+      # 最新の投稿
+      @latest_posts = Post.order(created_at: :desc).limit(20)
+      add_blank_post(@latest_posts)
     else
-      @posts = Post.order("RANDOM()").limit(20)
+      # ログイン前
+      @posts = Post.order("RANDOM()").limit(12)
+    end
+  end
+
+  private #################################
+  # 投稿数が20に満たない場合は空を追加
+  def add_blank_post(posts_ary)
+    while posts_ary.size < 20
+      posts_ary.push(Post.new)
     end
   end
 end

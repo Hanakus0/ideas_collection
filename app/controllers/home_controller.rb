74467c628b8cd3e53f27fd6b76992051752d48a4
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-06 11:35:37 +0900

    CHANGE: tagify install by npm -> yarn

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     # ログイン後
     if user_signed_in?
       # ランダム
-      @random_posts = Post.order("RANDOM()").limit(20)
+      @random_posts = Post.order("RANDOM()").limit(40)
       add_blank_post(@random_posts)
       # いいね数
       @good_posts = Post.find(PostLike.group(:post_id).order('count(post_id) desc').pluck(:post_id))
@@ -12,7 +12,7 @@ class HomeController < ApplicationController
       @comment_posts = Post.find(Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
       add_blank_post(@comment_posts)
       # 最新の投稿
-      @latest_posts = Post.order(created_at: :desc).limit(20)
+      @latest_posts = Post.order(created_at: :desc).limit(40)
       add_blank_post(@latest_posts)
     else
       # ログイン前
@@ -23,7 +23,7 @@ class HomeController < ApplicationController
   private #################################
   # 投稿数が20に満たない場合は空を追加
   def add_blank_post(posts_ary)
-    while posts_ary.size < 20
+    while posts_ary.size < 40
       posts_ary.push(Post.new)
     end
   end

e80b3eb99e7cb8759afa27b88b762b3281581932
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-08 00:16:45 +0900

    ADD: Ajax comment likes

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,18 +1,18 @@
 class CommentLikesController < ApplicationController
   # ブックマーク処理
   def create
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:id])
     @post = @comment.post
     current_user.liked_comments << @comment
-    redirect_to post_path(@post), success: t('.success')
+    # redirect_to post_path(@post), success: t('.success')
   end
 
   # ブックマーク解除処理
   def destroy
     # raise
+    @comment = Comment.find(params[:id])
     @post = Post.find_by(post_uid: params[:id])
-    @comment = Comment.find(params[:comment_id])
     current_user.liked_comments.destroy(@comment)
-    redirect_to post_path(@post), success: t('.success'), status: :see_other
+    # redirect_to post_path(@post), success: t('.success'), status: :see_other
   end
 end

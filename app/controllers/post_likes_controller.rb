ec36a91b47db8b6d03572bec113dee8f01b87341
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-07 23:10:07 +0900

    ADD: Ajax post like

--- app/controllers/post_likes_controller.rb
+++ app/controllers/post_likes_controller.rb
@@ -1,9 +1,9 @@
 class PostLikesController < ApplicationController
   # ブックマーク処理
   def create
-    @post = Post.find_by(post_uid: params[:post_uid])
+    @post = Post.find_by(post_uid: params[:id])
     current_user.like_posts << @post
-    redirect_to post_path(@post), success: t('.success')
+    # redirect_to post_path(@post), success: t('.success')
   end
 
   # ブックマーク解除処理
@@ -11,6 +11,6 @@ class PostLikesController < ApplicationController
     target_post = Post.find_by(post_uid: params[:id])
     @post = current_user.post_likes.find_by(post_id: target_post.id).post
     current_user.like_posts.destroy(@post)
-    redirect_to post_path(@post), success: t('.success'), status: :see_other
+    # redirect_to post_path(@post), success: t('.success'), status: :see_other
   end
 end

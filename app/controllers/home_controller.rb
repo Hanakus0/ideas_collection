58d390ede708761905c92f4a88afc8159287d4f5
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-10 12:53:35 +0900

    FIXED: to_a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,11 @@
 class HomeController < ApplicationController
+  before_action :set_posts_num
+
   def index
     # ログイン後
     if user_signed_in?
       # ランダム
-      @random_posts = Post.order("RANDOM()").limit(40)
+      @random_posts = Post.order("RANDOM()").limit(@posts_num)
       add_blank_post(@random_posts)
       # いいね数
       @good_posts = Post.find(PostLike.group(:post_id).order('count(post_id) desc').pluck(:post_id))
@@ -12,7 +14,7 @@ class HomeController < ApplicationController
       @comment_posts = Post.find(Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
       add_blank_post(@comment_posts)
       # 最新の投稿
-      @latest_posts = Post.order(created_at: :desc).limit(40)
+      @latest_posts = Post.order(created_at: :desc).limit(@posts_num)
       add_blank_post(@latest_posts)
     else
       # ログイン前
@@ -21,10 +23,15 @@ class HomeController < ApplicationController
   end
 
   private #################################
-  # 投稿数が20に満たない場合は空を追加
+  def set_posts_num
+    @posts_num = 100
+  end
+
+  # 投稿数が@posts_numに満たない場合は空を追加
   def add_blank_post(posts_ary)
-    while posts_ary.size < 40
-      posts_ary << Post.new
+    diff_num = @posts_num - posts_ary.size
+    diff_num.times do
+      posts_ary.to_a << Post.none
     end
   end
 end

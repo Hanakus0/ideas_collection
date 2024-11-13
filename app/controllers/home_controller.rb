bbdb81e91f9eef76a5867d6b83a23ff8c5256dba
Hanakus0 <everydoghas12day@outlook.jp>
2024-11-14 01:39:34 +0900

    REPAIR: home bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,10 @@ class HomeController < ApplicationController
       @random_posts = Post.where(draft_flg: 0).order("RANDOM()").limit(@posts_num)
       add_blank_post(@random_posts)
       # いいね数
-      @good_posts = Post.where(draft_flg: 0).find(PostLike.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+      @good_posts = Post.where(draft_flg: 0).where(id: PostLike.group(:post_id).order('count(post_id) desc').pluck(:post_id))
       add_blank_post(@good_posts)
       # コメント数
-      @comment_posts = Post.where(draft_flg: 0).find(Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+      @comment_posts = Post.where(draft_flg: 0).where(id: Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
       add_blank_post(@comment_posts)
       # 最新の投稿
       @latest_posts = Post.where(draft_flg: 0).order(created_at: :desc).limit(@posts_num)

ce825f16f8f6c60d61e4e4a5ba2cea86a07a0891
Hanakus0 <everydoghas12day@outlook.jp>
2024-12-02 15:17:18 +0900

    UPDATE: RSpec Model User refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,88 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:correct_user) { create(:correct_user) }
+  let(:correct_user2) { create(:correct_user) }
+  let(:build_correct_user) { build(:correct_user) }
+  let(:correct_file) { fixture_file_upload(Rails.root.join('spec/support/app_logo.png'), 'image/png') }
+
   describe '[Model : User] Success - バリデーションの確認' do
     # Signup
     context '適切な email と Password で新規登録する場合、' do
       it 'エラー無く特定の初期値でユーザー登録がされている' do
-        user = create(:correct_user)
-        # エラーが無い
-        expect(user).to be_valid
-        expect(user.errors).to be_empty
-        # 適切な値が入っている
-        expect(user.user_uid.length).to eq 20
-        expect(user.screen_name.length).to eq 16
-        expect(user.account_name).to eq '匿名さん'
-        expect(user.gender).to eq 'unknown'
-        expect(user.age).to eq 'unknown'
-        expect(user.introduction).to eq 'よろしくお願いいたします'
+        expect(correct_user).to be_valid
+        expect(correct_user.errors).to be_empty
+        expect(correct_user.user_uid.length).to eq 20
+        expect(correct_user.screen_name.length).to eq 16
+        expect(correct_user.account_name).to eq '匿名さん'
+        expect(correct_user.gender).to eq 'unknown'
+        expect(correct_user.age).to eq 'unknown'
+        expect(correct_user.introduction).to eq 'よろしくお願いいたします'
       end
       it 'email が他ユーザーと重複しなければエラー無くユーザー登録が出来る' do
-        user = create(:correct_user)
-        user2 = create(:correct_user)
-        expect(user.email).to match(/\A[^@\s]+@[^@\s]+\z/)
-        expect(user2.email).to match(/\A[^@\s]+@[^@\s]+\z/)
-        expect(user2).to be_valid
-        expect(user2.errors).to be_empty
+        expect(correct_user.email).to match(/\A[^@\s]+@[^@\s]+\z/)
+        expect(correct_user2.email).to match(/\A[^@\s]+@[^@\s]+\z/)
+        expect(correct_user2).to be_valid
+        expect(correct_user2.errors).to be_empty
       end
     end
     # Profile
     context '適切なプロフィール情報で編集する場合、' do
       it 'screen_name が文字数16文字以下の英数字なら登録可能' do
-        user = build(:correct_user)
-        # 英数字16文字以下
-        user.screen_name = 'abcdef1234567890'
-        user.save
-        expect(user.screen_name).to eq 'abcdef1234567890'
+        # 英数字16文字
+        build_correct_user.screen_name = 'abcdef1234567890'
+        build_correct_user.save
+        expect(build_correct_user.screen_name).to eq 'abcdef1234567890'
       end
       it 'screen_name が文字数5文字以上の英数字なら登録可能' do
-        user = build(:correct_user)
-        # 5文字以上
-        user.screen_name = 'abcde'
-        user.save
-        expect(user.screen_name).to eq 'abcde'
+        # 5文字
+        build_correct_user.screen_name = 'abcde'
+        build_correct_user.save
+        expect(build_correct_user.screen_name).to eq 'abcde'
       end
       it 'account_name が文字数20文字以下の文字列なら登録可能' do
-        user = build(:correct_user)
-        # 5文字以上
-        user.account_name = 'あいうえおaiueo12345/*-?!'
-        user.save
-        expect(user.account_name).to eq 'あいうえおaiueo12345/*-?!'
+        # 20文字
+        build_correct_user.account_name = 'あいうえおaiueo12345/*-?!'
+        build_correct_user.save
+        expect(build_correct_user.account_name).to eq 'あいうえおaiueo12345/*-?!'
       end
       it 'account_name が文字数1文字以上の文字列なら登録可能' do
-        user = build(:correct_user)
-        # 英数字16文字以下
-        user.account_name = 'あ'
-        user.save
-        expect(user.account_name).to eq 'あ'
+        # 1文字
+        build_correct_user.account_name = 'あ'
+        build_correct_user.save
+        expect(build_correct_user.account_name).to eq 'あ'
       end
       it 'introduction が未入力でも登録可能' do
-        user = build(:correct_user)
-        # 5文字以上
-        user.introduction = nil
-        user.save
-        expect(user.introduction).to eq nil
+        # 未入力
+        build_correct_user.introduction = nil
+        build_correct_user.save
+        expect(build_correct_user.introduction).to eq nil
       end
       it 'introduction が文字数160文字以下の文字列なら登録可能' do
-        user = build(:correct_user)
-        # 英数字16文字以下
-        user.introduction = 'ekqlfJBMwUSMZ1ifSrsoZRZD7A4k0vXpDBahPrjdYgMZL5GoyUg1FLufG5FQnJGLDNkNUBp2XoyvyWZwi8mEx1evRiRri4yOzZLNBIQNgF2BnNyNBQdfcCe9kA6QMkcZNK3qtnUehO3YI7BYJKZWAjW1TWohxTkw'
-        user.save
-        expect(user.introduction.length).to eq 160
+        # 160文字
+        build_correct_user.introduction = 'あいうえお12345かきくけこSMZ1ifSrsoZRZD7A4k0vXpDBahPrjdYgMZL5GoyUg1FLufG5FQnJGLDNkNUBp2XoyvyWZwi8mEx1evRiRri4yOzZLNBIQNgF2BnNyNBQdfcCe9kA6QMkcZNK3qtnUehO3YI7BYJKZWAjW1TWo'
+        build_correct_user.save
+        expect(build_correct_user.introduction.length).to eq 160
       end
       it 'profile_image はユーザー登録時には nil で登録される' do
-        user = create(:correct_user)
-        file = fixture_file_upload(Rails.root.join('spec/support/app_logo.png'), 'image/png')
-        expect(user.profile_image.url).not_to be_present
+        expect(correct_user.profile_image.url).not_to be_present
       end
       it 'profile_image は更新時に画像ファイル名をランダム文字列にしている' do
-        user = create(:correct_user)
-        file = fixture_file_upload(Rails.root.join('spec/support/app_logo.png'), 'image/png')
-        user.profile_image = file
-        user.save
-
-        expect(user.profile_image).to be_present
-        expect(user.profile_image.url).not_to include('app_logo.png')
+        correct_user.profile_image = correct_file
+        correct_user.save
+        expect(correct_user.profile_image.url).to be_present
+        expect(correct_user.profile_image.url).not_to include('app_logo.png')
       end
     end
   end

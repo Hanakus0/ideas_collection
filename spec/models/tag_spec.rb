5bf46f56871027e1563dfe6ea7f0a8f5b8979a94
Hanakus0 <everydoghas12day@outlook.jp>
2024-12-03 10:46:44 +0900

    ADD: RSpec Model: PostTag, PostGenre

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+    # Success
+  # ユーザー
+  let(:correct_user) { create(:correct_user) }
+  # ポストジャンル関連
+  let(:post_genre) { create(:post_genre) }
+  # 適切な投稿
+  let(:correct_post) { build(:post, post_genre: post_genre) }
+  # タグ関連
+  let(:tags) { create_list(:tag, 3) }
+
+  describe '[Model : Tag] Success - バリデーションの確認' do
+
+    context '適切な投稿内容で新規登録する場合、' do
+      it '必須項目ではない画像を未入力でも投稿できる' do
+        # タグを追加
+        correct_post.tags << tags
+
+        expect(correct_post).to be_present
+        expect(correct_post.tags.size).to eq 3
+        expect(correct_post.errors).to be_empty
+      end
+    end
+  end
 end

ede4b8ef0507ec96cbebbe0cf47ddfb104883466
Hanakus0 <everydoghas12day@outlook.jp>
2024-12-05 23:01:18 +0900

    ADD: rambulance Error pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,9 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  # config.consider_all_requests_local = true
+  config.consider_all_requests_local = true
   # 本番と同じようなエラーページを表示して確認したい場合
-  config.consider_all_requests_local = false
+  # config.consider_all_requests_local = false
 
   # Enable server timing.
   config.server_timing = true

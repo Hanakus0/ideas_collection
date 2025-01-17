066a22195a78cff3e22dde29aa3900c07f114e20
Hanakus0 <everydoghas12day@outlook.jp>
2025-01-17 20:15:05 +0900

    ADD: capybara-email

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe "ログイン前における操作", type: :system do
     end
   end
   # TODO: メール認証サインアップのテスト
+  context "メール認証サインアップのテスト" do
+    it "メール認証が成功しサインアップできる" do
+      visit new_user_registration_path
+      fill_in 'user_email', with: 'testuser@example.com'
+      fill_in 'user_password', with: 'password123'
+      fill_in 'user_password_confirmation', with: 'password123'
+      click_button 'アカウント登録'
+
+      open_email('testuser@example.com')
+      expect(current_email).to have_content '以下のリンクをクリックし、メールアドレスの確認手続を完了させてください。'
+      current_email.click_link '会員登録　メールアドレス認証'
+
+      switch_to_window(windows.last)
+      expect(page).to have_content('メールアドレスが確認できました。')
+    end
+  end
 end
